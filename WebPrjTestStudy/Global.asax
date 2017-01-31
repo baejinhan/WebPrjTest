3343d0c76a3e99b166315b3ace6cad482cc8d411
baejinhan <llovebboy@nate.com>
2017-01-31 10:35:23 +0900

    초기 커밋.

---
+++ WebPrjTestStudy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPrjTestStudy.MvcApplication" Language="C#" %>
---
+++ WebPrjTestStudy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPrjTestStudy.Startup))]
+namespace WebPrjTestStudy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
